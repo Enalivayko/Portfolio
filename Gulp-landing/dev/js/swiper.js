7dfd51ac97f9eef0e8eddc152ed9d89697954cf0
Enalivayko <evg.nalivayko@gmail.com>
2023-07-03 15:42:57 +0300

    gulp-landing

---
+++ Gulp-landing/dev/js/swiper.js
@@ -0,0 +1,12 @@
+'use strict';
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
